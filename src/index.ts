83505708a7621f0d6dd6b6e7c38b58bb6b252936
CiroLee <ciromail@126.com>
2023-01-20 13:23:34 +0800

    feat: add prettier to format code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 // 命令行彩色输出
-import chalk from "chalk";
-import yargs from "yargs";
+import chalk from 'chalk';
+import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
-import pkg from "../package.json";
+import pkg from '../package.json';
 // 命令行loading
 // import ora from "ora";
 // const spinner = ora({ spinner: "line" });
@@ -12,7 +12,6 @@ import pkg from "../package.json";
 // 允许自定义version和help
 const argv = yargs(hideBin(process.argv)).help(false).version(false);
 const parsedArgv = argv.parseSync();
-
 
 // 自定义help信息
 function showHelper() {
@@ -31,11 +30,10 @@ function showHelper() {
     process.exit(0);
   }
   if (h || help) {
-    showHelper()
-    process.exit(0)
+    showHelper();
+    process.exit(0);
   }
 
   // others
-  console.log(parsedArgv)
-  
+  console.log(parsedArgv);
 })();
